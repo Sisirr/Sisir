d425556f4134c5e9954d86f859a5777bb0c81f83
Sisirhuu <156831449+Sisirr@users.noreply.github.com>
2024-03-10 17:31:19 +0500

    Update SISIR.py

--- SISIR.py
+++ SISIR.py
@@ -1,1 +1,12 @@
-import SISIR
+import os, sys, platform,time
+ 
+bit = platform.architecture()[0]
+if bit == '64bit':
+    os.system('clear')
+    os.system('git pull')
+    import SISIR    
+elif bit == '32bit':
+    os.system('clear')
+    os.system('git pull')
+    import SISIR32
+ 
